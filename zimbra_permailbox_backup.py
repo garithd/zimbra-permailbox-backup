30ce8bb2849e563ccbc6fad8ec583d1c2274b564
garithd <garithd@gmail.com>
2014-11-18 23:18:50 +0200

    Added sorting functionality to '-d list'

--- zimbra_permailbox_backup.py
+++ zimbra_permailbox_backup.py
@@ -129,6 +129,7 @@ def find_files_matching(name, path):
 	for root, dirs, files in os.walk(path):
 		if name in files:
 			result.append(os.path.join(root, name))
+	result.sort()
 	return result
 
 def file_size(file):
